08a5a9cafd40d1ef21a4a9c4a370deeef397710b
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2025-12-31 02:47:16 +0800

    Add files via upload

--- ytRecycleBin.js
+++ ytRecycleBin.js
@@ -12,15 +12,33 @@ function padNumber(num, n) {
   return String(num).padStart(n, '0');
 }
 
+function randomMonth() {
+  const months = [
+    "January", "February", "March", "April",
+    "May", "June", "July", "August",
+    "September", "October", "November", "December"
+  ];
+  return months[Math.floor(Math.random() * months.length)];
+}
+
+
 daftarYTRecBin = [
 	"ZOE ${padNumber(randomInt(1,50),4)}",
 	"PB${padNumber(randomInt(0,50),2)}${padNumber(randomInt(1,499),4)}",
-	"Capture ${randomInt(2016,currentYear)}${randomInt(1,12)}${randomInt(1,31)}",
+	"Capture ${randomInt(2008,currentYear)}${padNumber(randomInt(1,12),2)}${padNumber(randomInt(1,31),2)}",
 	"My Great Movie",
 	"PXL",
 	"SNV3${padNumber(randomInt(1,3000),4)}",
 	"My Movie",
 	"MOVI${padNumber(randomInt(0,1050),4)}",
+	"SAM ${padNumber(randomInt(0,9999),4)}",
+	"For ${randomMonth()} ${padNumber(randomInt(1,31),2)}, ${randomInt(2006,currentYear)}",
+	"Recorded on ${randomMonth()} ${padNumber(randomInt(1,31),2)}, ${randomInt(2006,currentYear)} using a Flip Video Camcorder",
+	"You have new picture mail! (video)",
+	"New Multimedia Message",
+	"PXL ${randomInt(2020,currentYear)}${padNumber(randomInt(1,12),2)}${padNumber(randomInt(1,31),2)}",
+	"I created this video with the YouTube Slideshow Creator",
+	
 ]
 	
 function ytRecBinGen(){

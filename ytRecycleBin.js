b284cc65159b978b239f34b58752bcbe32c5506e
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2025-12-31 21:16:59 +0800

    Add files via upload

--- ytRecycleBin.js
+++ ytRecycleBin.js
@@ -21,6 +21,17 @@ function randomMonth() {
   return months[Math.floor(Math.random() * months.length)];
 }
 
+function randomHex(length = 6, letterCase = "lower") {
+  const chars = "0123456789abcdef";
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    result += chars[Math.floor(Math.random() * 16)];
+  }
+
+  return letterCase === "upper" ? result.toUpperCase() : result;
+}
+
 
 daftarYTRecBin = [
 	"ZOE ${padNumber(randomInt(1,50),4)}",
@@ -41,10 +52,50 @@ daftarYTRecBin = [
 	"MOV${padNumber(randomInt(0,10),5)}",
 	"Recording gvo",
 	"My Videolicious Video",
+	"${yearRand}${padNumber(randomInt(1,12),2)}${padNumber(randomInt(1,31),2)}",
+	"I created this video with the YouTube Video Editor",
+	"FHD${padNumber(randomInt(1,1000),4)}",
+	"M4V${padNumber(randomInt(0,9999),5)}",
+	"Media1.3gp",
+	"DSC ${padNumber(randomInt(0,5000),5)}",
+	"Javaw",
+	"MAV ${padNumber(randomInt(0,399),4)}",
+	"480p 2000k",
+	"100 ${padNumber(randomInt(0,9999),4)}",
+	"/Storage/Emulated/",
+	"Sequence ${padNumber(randomInt(1,200),3)}",
+	"DSCN${padNumber(randomInt(0,9999),4)}",
+	"RPReplay",
+	"HDV ${padNumber(randomInt(1,5000),4)}",
+	"WA${padNumber(randomInt(0,999),4)}",
+	"WhatsApp Video ${yearRand}",
+	"GOPR${padNumber(randomInt(0,9999),4)}",
+	"Vid${padNumber(randomInt(0,10),3)}",
+	"muuvee${padNumber(randomInt(0,40),4)}",
+	"Lv 0",
+	"Sent from my blackberry smartphone",
+	"4${randomHex(3, 'upper')}",
+	"C${padNumber(randomInt(0,9999),4)}",
+	"WIN ${yearRand}${padNumber(randomInt(1,12),2)}${padNumber(randomInt(1,31),2)}",
+	"DSCF${padNumber(randomInt(0,9999),4)}",
+	".3g2",
+	"ZOOM${padNumber(randomInt(1,800),4)}",
+	"Kazam Screencast ${padNumber(randomInt(1,50),5)}",
+	"AVSEQ${padNumber(randomInt(0,99),2)}.DAT ",
+	"Video Assignment",
+	"GMT${yearRand}${padNumber(randomInt(1,12),2)}${padNumber(randomInt(1,31),2)}",
+	"VICO${padNumber(randomInt(1,250),4)}",
+	"XRecorder ${padNumber(randomInt(1,31),2)}${padNumber(randomInt(1,12),2)}${randomInt(2021,2024)}",
+	"Bandicam ${randomInt(2010,currentYear)} ${padNumber(randomInt(1,12),2)} ${padNumber(randomInt(1,31),2)}",
+	"FullSizeRender",
+	"DSC ${padNumber(randomInt(0,9999),4)}",
+	"My First Project",
 	
 ]
 	
 function ytRecBinGen(){
 	const currentYear = new Date().getFullYear();
+	let yearRand = 0 + currentYear;
+	while(Math.random() < 0.5){yearRand = yearRand - 1;}
 	return eval("`"+daftarYTRecBin[Math.floor(Math.random()*daftarYTRecBin.length)]+"`");
 }
